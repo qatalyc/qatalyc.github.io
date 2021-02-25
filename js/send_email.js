8e42815ee241be26b156134a578d3471ef457682
Anurag Sharma <anssharma61@gmail.com>
2021-02-25 13:57:13 +0530

    New website files

--- js/send_email.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-        url: 'https://api.sendgrid.com/v3/mail/send',
-        beforeSend: function(xhr) {
-             xhr.setRequestHeader("Authorization", "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")
-        }, success: function(data){
-            alert(data);
-            //process the JSON data etc
-        }
-})
